766c7c893286c5b3119bcd7e70223f703642c311
KPVBY <kpvbygm@gmail.com>
2019-11-13 17:50:56 +0300

    added event listener for scroll, loading messages on scroll is done

--- src/index.js
+++ src/index.js
@@ -4,18 +4,30 @@ import {DATA} from './data.js';
 
 function component() {
     let i;
+    let b=20;
     let form = document.querySelector(".form-list_message");
     let app = document.querySelector(".form-list");
-    for (i = 0; i < 19; i++) {
+    for (i = 0; i < b; i++) {
         let inputMessages = templateList({content: DATA[i]});
         form.append(inputMessages);
     }
     const scrollLoading = (event) => {
         let {scrollHeight, scrollTop, offsetHeight} = event.target;
-        console.log(scrollHeight, scrollTop, offsetHeight)
+        console.log(scrollHeight, scrollTop, offsetHeight);
+        if (scrollHeight < scrollTop + offsetHeight) {
+            for (; i < b + b; i++) {
+                if(i>=DATA.length){
+                    break
+                }
+                let inputMessages = templateList({content: DATA[i]});
+                form.append(inputMessages);
+            }
+            b=b+20;
+        }
+
     };
     app.addEventListener('scroll', scrollLoading);
-    
+
     return app;
 }
 
