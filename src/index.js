f6c69cdf9900bcbb9942b17b34fe62cff5de65bd
KPVBY <kpvbygm@gmail.com>
2019-11-15 18:14:31 +0300

    using search function for sorting strings

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import {DATA} from './data.js';
 function component() {
     let b = 19;
     let c = 40;
+    let arr = [];
     let form = document.querySelector(".form-list_message");
     let app = document.querySelector(".form-list");
+    let input = document.querySelector(".inputData");
 
     DATA.forEach((i, element) => {
         if (element <= b) {
@@ -17,22 +19,33 @@ function component() {
 
     const scrollLoading = (event) => {
         let {scrollHeight, scrollTop, offsetHeight} = event.target;
-        console.log(scrollHeight, scrollTop, offsetHeight);
+        // console.log(scrollHeight, scrollTop, offsetHeight);
         if (scrollHeight < scrollTop + offsetHeight) {
 
             DATA.forEach((i, element) => {
-                if(element>b && element<c){
-                    let inputMessages = templateList({content: DATA[element]});
-                    form.append(inputMessages);
+                if (element > b && element < c) {
+                    form.append(templateList({content: DATA[element]}));
                 }
             });
-            b=c;
-            c+=20;
+            b = c;
+            c += 20;
         }
-
+    };
+    const getMessages = () => {
+        let newArray = input.value.trim().toLowerCase();
+        let elementsForm = document.querySelectorAll(".form-list_message .form-list_message_number");
+        elementsForm.forEach((elem) => {
+            if (elem.innerHTML.toLowerCase().search(newArray.trim()) === -1) {
+                elem.classList.add('hide');
+            } else {
+                elementsForm.forEach((elem) => {
+                    elem.classList.remove('hide');
+                })
+            }
+        });
     };
     app.addEventListener('scroll', scrollLoading);
-
+    input.addEventListener('keyup', getMessages);
     return app;
 }
 
